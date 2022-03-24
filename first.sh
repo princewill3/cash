c590f346cce6b768f9d9153e43cb42397ea2543d
princewill3 <99935794+princewill3@users.noreply.github.com>
2022-03-23 22:48:15 -0400

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,2 +1,2 @@
 echo "your first assignment is going to be a devops engineer"
-echo "God is with me through this journey, i believe in hios saving grace"
+echo "God is with me through this journey, i believe in his saving grace"
